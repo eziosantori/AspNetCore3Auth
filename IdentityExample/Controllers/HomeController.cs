04671fba93afc3a8143183f30fb06eca02cafd70
Ezio Santori <Ezio.Santori@wolterskluwer.com>
2020-09-11 15:11:46 +0200

    Email Verification PaperCut nont working

--- IdentityExample/Controllers/HomeController.cs
+++ IdentityExample/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NETCore.MailKit.Core;
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -13,14 +14,17 @@ namespace IdentityExample.Controllers
   {
     private readonly UserManager<IdentityUser> _userManager;
     private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly IEmailService _emailService;
 
     public HomeController(
       UserManager<IdentityUser> userManager,
-      SignInManager<IdentityUser> signInManager
+      SignInManager<IdentityUser> signInManager,
+      IEmailService emailService
       )
     {
       _userManager = userManager;
       _signInManager = signInManager;
+      _emailService = emailService;
     }
     public IActionResult Index()
     {
@@ -61,20 +65,47 @@ namespace IdentityExample.Controllers
     [HttpPost]
     public async Task<IActionResult> Register(string username, string password)
     {
-      var user = new IdentityUser(username);
+      //register functionality
+
+      var user = new IdentityUser
+      {
+        UserName = username,
+        Email = "",
+      };
+
       var result = await _userManager.CreateAsync(user, password);
 
       if (result.Succeeded)
       {
-        // signIn User here
-        //sigin
-        var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
-        if (signInResult.Succeeded)
-          return RedirectToAction("Index");
+        //generation of the email token
+        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+        var link = Url.Action(nameof(VerifyEmail), "Home", new { userId = user.Id, code }, Request.Scheme, Request.Host.ToString());
+
+        await _emailService.SendAsync("test@test.com", "email verify", $"<a href=\"{link}\">Verify Email</a>", true);
+
+        return RedirectToAction("EmailVerification");
       }
+
       return RedirectToAction("Index");
     }
+    public async Task<IActionResult> VerifyEmail(string userId, string code)
+    {
+      var user = await _userManager.FindByIdAsync(userId);
+      if (user == null) return BadRequest();
 
+      var result = await _userManager.ConfirmEmailAsync(user, code);
+      if (result.Succeeded)
+      {
+        return View();
+      }
+
+      return BadRequest();
+;    }
+      public IActionResult EmailVerification()
+    {
+      return View();
+    }
     public async Task<IActionResult> LogOut()
     {
       await _signInManager.SignOutAsync();
--- IdentityExample/Startup.cs
+++ IdentityExample/Startup.cs
@@ -8,43 +8,52 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NETCore.MailKit.Extensions;
+using NETCore.MailKit.Infrastructure.Internal;
 
 namespace IdentityExample
 {
   public class Startup
   {
+    private readonly IConfiguration _config;
+
+    public Startup(IConfiguration config)
+    {
+      _config = config;
+    }
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<AppDbContext>( config => {
+      services.AddDbContext<AppDbContext>(config =>
+      {
         config.UseInMemoryDatabase("Memory");
       });
 
-      services.AddSingleton<AppDbContext>();
-
-      services.AddIdentity<IdentityUser, IdentityRole>(config=> {
+      // AddIdentity registers the services
+      services.AddIdentity<IdentityUser, IdentityRole>(config =>
+      {
         config.Password.RequiredLength = 4;
         config.Password.RequireDigit = false;
         config.Password.RequireNonAlphanumeric = false;
         config.Password.RequireUppercase = false;
+        config.SignIn.RequireConfirmedEmail = true;
       })
-        .AddEntityFrameworkStores<AppDbContext>()
-        .AddDefaultTokenProviders();
+          .AddEntityFrameworkStores<AppDbContext>()
+          .AddDefaultTokenProviders();
 
-      services.ConfigureApplicationCookie(config => {
-        config.Cookie.Name = "Identity.Coockie";
+      services.ConfigureApplicationCookie(config =>
+      {
+        config.Cookie.Name = "Identity.Cookie";
         config.LoginPath = "/Home/Login";
       });
-      //services.AddAuthentication("CookieAuth")
-      //  .AddCookie("CookieAuth", config => {
-      //    config.Cookie.Name = "Grandmas.Coockie";
-      //    config.LoginPath = "/Home/Authenticate";
-      //  });
 
-      //services.AddControllersWithViews();
+      services.AddMailKit(config => config.UseMailKit(_config.GetSection("Email").Get<MailKitOptions>()));
+
+      services.AddControllersWithViews();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
